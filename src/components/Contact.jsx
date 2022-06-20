f623b7f8680cd2af7c9dd9e35a35b3be4b240408
andi-prasetyo <andiprasetyo700@gmail.com>
2022-06-20 08:23:12 +0700

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,27 @@
+// Terima props lalu tampilkan dalam Contact component
+// Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import React from 'react';
+import style from "./Contact.css";
+
+
+const Contact = ({data}) => {
+    const { name, email, phone, photo } = data;
+    return (
+        <div className={style.contact}>
+            <div className={style.contact__avatar}>
+                <img src={photo} alt={name} />
+            </div>
+            <div className={style.contact__info}>
+                <p className={style.contact__info__txt}>
+                    <b>{name}</b>
+                </p>
+                <div className={style.contact__infodesc}>
+                    <p className={style.contact__infodesc__txt}>{phone}</p>
+                    <p className={style.contact__infodesc__txt}>{email}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// Kalian bisa menambahkan CSS di src/components/Header.css
+import React from 'react';
+import style from "./Header.css";
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            <h1 className={style.header__title}>Call a Friend</h1>
+            <p className={style.header__subtitle}>your friendly contact app </p>
+        </header>
+    )
+}
+
+export default Header;
